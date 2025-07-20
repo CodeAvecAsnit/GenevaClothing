91d4ea2eb03ddafdf07795270f2dbc08b57e2b60
CodeAvecAsnit <asnitbakhati@gmail.com>
2025-07-20 21:43:20 +0545

    Resolved Merge Conflicts

--- GenevaEcommerceJava/src/main/java/com/ecomm/np/genevaecommerce/Controllers/CollectionsController.java
+++ GenevaEcommerceJava/src/main/java/com/ecomm/np/genevaecommerce/Controllers/CollectionsController.java
@@ -1,27 +1,17 @@
 package com.ecomm.np.genevaecommerce.Controllers;
 
-<<<<<<< HEAD
 import com.ecomm.np.genevaecommerce.DTO.CollectionAndItemsDTO;
 import com.ecomm.np.genevaecommerce.Mail.MailService;
-=======
-
-import com.ecomm.np.genevaecommerce.DTO.CollectionAndItemsDTO;
-
-
->>>>>>> 9f256d61dc9a90092f4457cfedfe66a540c77636
 import com.ecomm.np.genevaecommerce.Models.BestCollection;
 import com.ecomm.np.genevaecommerce.Models.Collection;
 import com.ecomm.np.genevaecommerce.Services.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.io.IOException;
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
-=======
->>>>>>> 9f256d61dc9a90092f4457cfedfe66a540c77636
-
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -29,66 +19,53 @@ import java.util.List;
 public class CollectionsController {
 
     private final HomeService homeService;
-<<<<<<< HEAD
     private final MailService mailService;
 
     @Autowired
-    public CollectionsController(HomeService homeService,MailService mailService){
+    public CollectionsController(HomeService homeService, MailService mailService) {
         this.homeService = homeService;
         this.mailService = mailService;
-=======
-
-    @Autowired
-    public CollectionsController(HomeService homeService){
-        this.homeService = homeService;
->>>>>>> 9f256d61dc9a90092f4457cfedfe66a540c77636
     }
 
     @GetMapping
-    public ResponseEntity<CollectionAndItemsDTO> provideCollection(){
+    public ResponseEntity<CollectionAndItemsDTO> provideCollection() {
         Collection collection = homeService.saveCollection();
         return ResponseEntity.ok(CollectionAndItemsDTO.buildFromCollection(collection));
     }
 
     @GetMapping("/best")
-    public ResponseEntity<BestCollection> getBest(){
-        try{
+    public ResponseEntity<BestCollection> getBest() {
+        try {
             return ResponseEntity.ok(homeService.bestCollection());
         } catch (IOException e) {
             return ResponseEntity.badRequest().body(null);
         }
     }
 
-<<<<<<< HEAD
     @PostMapping("/post/best")
-=======
-    @PostMapping
->>>>>>> 9f256d61dc9a90092f4457cfedfe66a540c77636
-    public ResponseEntity<String> updateBest(@RequestBody BestCollection collection){
-        try{
+    public ResponseEntity<String> updateBest(@RequestBody BestCollection collection) {
+        try {
             return ResponseEntity.ok(homeService.updateBestCollection(collection));
         } catch (IOException e) {
             return ResponseEntity.badRequest().body("Cannot update");
         }
     }
-<<<<<<< HEAD
 
     @GetMapping("/code")
-    public ResponseEntity<Integer> sendCode(@RequestBody String email){
+    public ResponseEntity<Integer> sendCode(@RequestParam String email) {
         return ResponseEntity.ok(mailService.sendVerificationCode(email));
     }
 
     @GetMapping("/batch")
-    public int sendMails(){
+    public int sendMails() {
         List<String> mailList = new ArrayList<>();
         mailList.add("asnitbakhati@gmail.com");
         mailList.add("aranzabakhati@gmail.com");
         mailList.add("csit23081031_asnit@achsnepal.edu.np");
-        String title = "Ill sont une Test";
-        String body = "Bonjour. Ill sone une test pour mail sevice.";
-        mailService.sendPromotionalEmail(mailList,title,body);
+
+        String title = "Ils sont un test";
+        String body = "Bonjour. Ceci est un test pour le service de mail.";
+        mailService.sendPromotionalEmail(mailList, title, body);
         return 1;
     }
-=======
->>>>>>> 9f256d61dc9a90092f4457cfedfe66a540c77636
 }
